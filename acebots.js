48d3ff9844023cc5638deeb09cc1b02eb76a4bbe
Glitch (gifted-fine-guarantee) <none>
2021-02-27 23:01:14 +0000

    🌶💅 Checkpoint ./acebots.js:26490528/37

--- acebots.js
+++ acebots.js
@@ -313,20 +313,20 @@ client.on("message", async message => {
 
 const AntiSpam = require('discord-anti-spam');
 const antiSpam = new AntiSpam({
-	warnThreshold: 6, // Amount of messages sent in a row that will cause a warning.
+	warnThreshold: 4, // Amount of messages sent in a row that will cause a warning.
 	kickThreshold: 8, // Amount of messages sent in a row that will cause a ban.
 	banThreshold: 12, // Amount of messages sent in a row that will cause a ban.
 	maxInterval: 3000, // Amount of time (in milliseconds) in which messages are considered spam.
 	warnMessage: '{@user}, Flood/spam yapmayı kes yoksa sunucudan atılacaksın.', // Message that will be sent in chat upon warning a user.
 	kickMessage: '**{user_tag}** adlı kullanıcı flood/spam dolayı kicklendi.', // Message that will be sent in chat upon kicking a user.
 	banMessage: '**{user_tag}** adlı kullanıcı flood/spam dolayı banlandı.', // Message that will be sent in chat upon banning a user.
-	maxDuplicatesWarning: 5, // Amount of duplicate messages that trigger a warning.
-	maxDuplicatesKick: 6, // Amount of duplicate messages that trigger a warning.
+	maxDuplicatesWarning: 4, // Amount of duplicate messages that trigger a warning.
+	maxDuplicatesKick: 8, // Amount of duplicate messages that trigger a warning.
 	maxDuplicatesBan: 12, // Amount of duplicate messages that trigger a warning.
 	exemptPermissions: ['ADMINISTRATOR'], // Bypass users with any of these permissions.
 	ignoreBots: true, // Ignore bot messages.
 	verbose: true, // Extended Logs from module.
-	ignoredUsers: [], // Array of User IDs that get ignored.
+	ignoredUsers: ["776837094962102282","","",], // Array of User IDs that get ignored.
 	// And many more options... See the documentation.
 });
 
