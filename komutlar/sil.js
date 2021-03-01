26f6dddc96facad4b29e3908f7edc1a2d993e366
Glitch (gifted-fine-guarantee) <none>
2021-03-01 13:03:53 +0000

    🎑💷 Checkpoint ./komutlar/sil.js:26490528/180

--- komutlar/sil.js
+++ komutlar/sil.js
@@ -16,7 +16,7 @@ exports.run = function(client, message, args) {
 if (!message.member.roles.cache.get(yetkili) & !message.member.hasPermission("ADMINISTRATOR")) return message.react(basarisiz);
   
 if(isNaN(args[0])) {
-  var errembed = new Discord.MessageEmbed().setColor("RED").setDescription(`<a:qmi2:809010861162233857> Lütfen 1-100 arasında sayı belirtiniz!`)
+  var errembed = new Discord.MessageEmbed().setColor("RED").setDescription(`<a:plantacarp:815252488168931368> Lütfen 1-100 arasında sayı belirtiniz!`)
 return message.channel.send(errembed).then(x => x.delete({timeout: 3000}));
 }
   
@@ -24,9 +24,9 @@ if (args[0] < 1) return message.channel.send(new Discord.MessageEmbed().setColor
 if (args[0] > 100) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription("<a:plantacarp:815252488168931368> **100** adetten fazla mesaj silemem!")).then(x => x.delete({timeout: 3000}));
   
 message.channel.bulkDelete(args[0]).then(deletedMessages => {
-if (deletedMessages.size < 1) return message.reply("<a:qmi2:809010861162233857> Hiç mesaj silemedim! _(**14** günden önceki mesajları silemem!)_").then(x => x.delete({timeout: 3000}));
+if (deletedMessages.size < 1) return message.reply("<a:plantacarp:815252488168931368> Hiç mesaj silemedim! _(**14** günden önceki mesajları silemem!)_").then(x => x.delete({timeout: 3000}));
 })
-message.channel.send(new Discord.MessageEmbed().setColor('GREEN').setDescription(`<a:qmi1:809010834146983966> **${args[0]}** adet mesaj başarıyla silindi!`)).then(x => x.delete({timeout: 3000}));
+message.channel.send(new Discord.MessageEmbed().setColor('GREEN').setDescription(`<a:plantatik3:810180493424721921> **${args[0]}** adet mesaj başarıyla silindi!`)).then(x => x.delete({timeout: 3000}));
 };
 
 exports.conf = {
