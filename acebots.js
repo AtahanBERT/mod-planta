f8dac1a801d6c69d0da091bb2533872e91a23fe5
Glitch (gifted-fine-guarantee) <none>
2021-03-08 17:24:25 +0000

    ☂️🐴 Checkpoint ./acebots.js:26490528/3

--- acebots.js
+++ acebots.js
@@ -332,7 +332,7 @@ const antiSpam = new AntiSpam({
 
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}.`));
 
-client.off('message', (message) => antiSpam.message(message));
+client.on('message', (message) => antiSpam.message(message));
 
 
 //-------------------------------------------------------------------------\\\ModLog//----------------------------------------------------------------------------------\\
