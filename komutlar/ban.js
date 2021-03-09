b8b1a9de89e234704162d3429d94e05f6b79064c
Glitch (gifted-fine-guarantee) <none>
2021-03-09 21:31:11 +0000

    🍘🎄 Checkpoint ./komutlar/ban.js:26490528/383

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -10,6 +10,7 @@ exports.run = async(client, message, args) => {
 }
   
     let yetkili = ayarlar.banyetkili;
+    let member = message.guild.member(user);
     let banlogkanal = ayarlar.banlog;
     let bansayı = db.fetch(`bansayısı_${message.author.id}`);
     let user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
@@ -20,10 +21,11 @@ exports.run = async(client, message, args) => {
     if (!message.member.roles.cache.get(yetkili) & !message.member.hasPermission("ADMINISTRATOR")) return message.react(basarisiz);
     if(!user) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:plantacarp:815252488168931368> Banlamak için birisini etiketlemelisin!`)).then(x => x.delete({timeout: 3000}));
     if(!sebep) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:plantacarp:815252488168931368> Banlamak için bir sebep belirtmelisin!`)).then(x => x.delete({timeout: 3000}));
+    if(message.member.roles.highest.position <= member.roles.highest.position) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:plantacarp:815252488168931368> Belirttiğin kişi senden üstün veya onunla aynı yetkidesin!`)).then(x => x.delete({timeout: 3000}));
     if(user.id === message.author.id) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription('<a:plantacarp:815252488168931368> Kendini banlayamazsın.')).then(x => x.delete({timeout: 3000}));
     if(user.id === client.user.id) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription('<a:plantacarp:815252488168931368> Botu banlayamazsın.')).then(x => x.delete({timeout: 3000}));
     if(user.id === message.guild.ownerID) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription ('<a:plantacarp:815252488168931368> Sunucu sahibini banlayamazsın.')).then(x => x.delete({timeout: 3000}));
-    if (!ayarlar.sahip) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:plantacarp:815252488168931368> Sahibimin üzerinde komut kullanamazsın!`));
+    if(!ayarlar.sahip) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:plantacarp:815252488168931368> Sahibimin üzerinde komut kullanamazsın!`));
 
 
    message.guild.members.cache.get(user.id).ban({reason: `${sebep}`});
@@ -41,9 +43,9 @@ exports.conf = {
 	enabled: true,
 	guildOnly: false,
 	aliases:[],
-	permlevel: 5,
+	permlevel: 2,
 };
 
 exports.help = {
-	name: ""
+	name: "ban"
 }
