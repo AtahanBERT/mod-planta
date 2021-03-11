75587b0f2990e8617c2078c4d4cf571a8f5fe7a7
Glitch (gifted-fine-guarantee) <none>
2021-03-11 10:10:50 +0000

    🚜🏚 Checkpoint ./komutlar/kayıt-kız.js:26490528/2020 ./komutlar/isim.js:26490528/3795 ./komutlar/kayıt-erkek.js:26490528/5 ./ayarlar.json:26490528/74

--- komutlar/kayıt-kız.js
+++ komutlar/kayıt-kız.js
@@ -1,5 +1,5 @@
-const discord = require('discord.js');
-const db = require('quick.db');
+const discord = require('discord.js')
+const db = require('quick.db')
 const ayarlar = require('../ayarlar.json');
 
  
@@ -7,20 +7,20 @@ const ayarlar = require('../ayarlar.json');
 
 exports.run = async (client, message, args) => {
   
-    let erkekrolcuk = ayarlar.erkekrol1;
-    let erkekrolcuk2 = ayarlar.erkekrol2;
-    let erkekrolcuk3 = ayarlar.erkekrol3;
-    let basarili = ayarlar.basariliemoji;
-    let basarisiz = ayarlar.basarisizemoji;
+    let erkekrolcuk = ayarlar.kızrol1;
+    let erkekrolcuk2 = ayarlar.kızrol2;
+    let erkekrolcuk3 = ayarlar.kızrol3;
     let yetkili = ayarlar.kayıtyetkili;
     let kayıtsız = ayarlar.kayıtsız;
+    let basarili = ayarlar.basariliemoji;
+    let basarisiz = ayarlar.basarisizemoji;
     let kayıtsayı = db.fetch(`kayıtsayı_${message.author.id}`);
     let erkekkayıt = db.fetch(`erkekkayıtsayı_${message.author.id}`);
     let genelsohbet1 = ayarlar.genelsohbet;
     let kayıtsohbet2 = ayarlar.kayıtsohbet;
-    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-    let isim = args[1]; 
-    let yaş = args[2]; 
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    let isim = args[1]; //acebots 
+    let yaş = args[2];
   
    if(db.fetch(`bakim`)) {
   if(message.author.id !== ayarlar.sahip) {return message.channel.send(new discord.MessageEmbed().setColor('RED').setDescription(`${basarisiz} Bot kullanımı şuanda kapalıdır. Lütfen sonra tekrar deneyiniz!`))}
@@ -31,7 +31,7 @@ exports.run = async (client, message, args) => {
 
 
  if (!message.member.roles.cache.get(yetkili) & !message.member.hasPermission("ADMINISTRATOR")) return message.react(basarisiz);
- if (!member) return message.channel.send(new discord.MessageEmbed().setColor('RED').setDescription(`${basarisiz} Erkek olarak kaydedeceğin kullanıcıyı belirtmelisin!`));
+ if (!member) return message.channel.send(new discord.MessageEmbed().setColor('RED').setDescription(`${basarisiz} Kız olarak kaydedeceğin kullanıcıyı belirtmelisin!`));
  if (!ayarlar.sahip) return message.channel.send(new discord.MessageEmbed().setColor('RED').setDescription(`${basarisiz} Sahibimin üzerinde komut kullanamazsın!`));
 
   member.roles.remove(kayıtsız);
@@ -42,7 +42,6 @@ exports.run = async (client, message, args) => {
 
   
 message.react(basarili)
-
 };
 exports.conf = {
     enabled: true,
@@ -52,5 +51,5 @@ exports.conf = {
 };
 
 exports.help = {
-    name: "erkek"
+    name: ""
 }
