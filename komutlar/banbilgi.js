ad31546504fae07a16e0f17dd6525d1abe40fe4b
Glitch (gifted-fine-guarantee) <none>
2021-03-12 15:38:07 +0000

    🐉🍛 Checkpoint ./komutlar/banbilgi.js:26490528/26 ./komutlar/sjail.js:26490528/39 ./komutlar/mute.js:26490528/12817

--- komutlar/banbilgi.js
+++ komutlar/banbilgi.js
@@ -5,13 +5,13 @@ exports.run = async (client, message, args) => {
 //-------------------------------------------------------------------------------\\
   
 if(!["789637815268933673"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) 
-return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL()({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL()({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
 
 //-------------------------------------------------------------------------------\\
 
   let basarisiz = ayarlar.basarisizemoji
   let basari = ayarlar.basariliemoji
-  if(!args[0]) return message.channel.send(new MessageEmbed().setDescription(`${message.author} bir ID belirtmelisin.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+  if(!args[0]) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author} bir ID belirtmelisin.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
   try {
     message.guild.fetchBan(args.slice(0).join(' '))
     .then(({ user, reason }) => message.channel.send(new MessageEmbed().setAuthor(user.tag, user.avatarURL()).setColor('0x348f36').addField('Banlanan Kullanıcı', `**${user.tag}** \`(${user.id})\``).setDescription(`**Ban Sebebi:** \`${reason}\``)))
