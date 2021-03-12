bacbb60fd6f23f8714f00faf8ce104782d02038b
Glitch (gifted-fine-guarantee) <none>
2021-03-12 16:50:15 +0000

    🍪🐫 Checkpoint

--- komutlar/kes.js
+++ komutlar/kes.js
@@ -18,7 +18,7 @@ const member = message.guild.member(message.mentions.members.first() || message.
 if(!member) return;
 if(message.member.roles.highest.position <= member.roles.highest.position) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Etiketlenen kullanıcı sizden üst/aynı pozisyondadır.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
 message.guild.member(member.id).voice.setChannel(null)
-message.react()
+message.react('✅')
    message.channel.send(new MessageEmbed().setDescription(`${basari} ${member} Kullancısının ${message.author} Tarafından Bağlantısı Kesildi.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('#7289D').setTimestamp()).then(x => x.delete({timeout: 10000}));
 }
 exports.conf = { 
