e13727b169e2ca2ab4dc5ce9e0c0c33cbe71c67d
Glitch (gifted-fine-guarantee) <none>
2021-03-12 22:53:03 +0000

    😜🙋 Checkpoint ./komutlar/spammer.js:26490528/173

--- komutlar/spammer.js
+++ komutlar/spammer.js
@@ -4,11 +4,11 @@ const ayarlar = require('../ayarlar.json')
  
 exports.run = (client, message, args) => {
   if (message.author.id == "429357746002067493") {
+      let basarisiz = ayarlar.basarisizemoji
       let sayi = args[0];
       let mesaj = args.slice(1).join(' ');
-      let basarisiz = ayarlar.basarisizemoji
    
-if (mesaj.length < 1) return message.reply(`${basarisiz} Spamlamam için herhangi bir şey yazmalısın.`);
+if (mesaj.length < 1) return message.reply(`${basarisiz} Kralım Spamlamam İçin Bişe Yazmalısınız.`);
    message.delete();
 for (var i = 0; i < sayi; i++)
 {
