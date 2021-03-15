260053a6aae313273ebd1fa63cff133dcce556e4
Glitch (planta-mod) <none>
2021-03-15 19:40:45 +0000

    ✂️📡 Checkpoint ./events/ready.js:26490528/1

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ const prefix = ayarlar.prefix;
 const ayarlar = require("../ayarlar.json");
 
 module.exports = client => {
-  let durum = ayarlar.durum;
+  let durum = ayarlar.durum
   client.user.setActivity(durum,  { type: 'PLAYING' });
 };
