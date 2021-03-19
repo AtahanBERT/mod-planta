b4ac258dc03bad93b28b3e1877896d91f5b319ba
Glitch (planta-mod) <none>
2021-03-19 16:54:05 +0000

    🐯🐜 Checkpoint ./komutlar/isim.js:26490528/3087

--- komutlar/isim.js
+++ komutlar/isim.js
@@ -2,21 +2,21 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const ayarlar = require('../ayarlar.json');
 const prefix = ayarlar.prefix
-let basarisiz = ayarlar.basarisizemoji;
 let basari = ayarlar.basariliemoji;
+let basarisiz = ayarlar.basarisizemoji;
 
 exports.run = async (client, message, args) => {
 
-if (!message.member.roles.cache.get(ayarlar.kayıtyetkili) & !message.member.hasPermission("ADMINISTRATOR")) return message.reply(`${basarisiz} Bu Komutu Kullanmak İçin **Yetkili** Olmalısın!`);
+if (!message.member.roles.cache.get(ayarlar.kayıtyetkili)) return message.reply(`Bu Komutu Kullanmak İçin **Yetkili** Olmalısın!`); 
 
 let tag = ayarlar.tag;
 let isim = args.slice(1).join(' ');
 let kullanici = message.mentions.users.first();
   
-if(!kullanici) return message.reply(`${basarisiz} Lütfen bir kullanıcı giriniz! \nDoğru Kullanım; \`${prefix}isimdeğiştir @${client.user.username}#${client.user.discriminator} <yeni isim>\``)
+if(!kullanici) return message.reply(`${basarisiz} Lütfen bir kullanıcı giriniz! \nDoğru Kullanım; \`${prefix}isim @üye <yeni isim>\``)
   
-if(!isim) return message.reply(`${basarisiz} Lütfen bir kullanıcı adı giriniz! \nDoğru Kullanım; \`${prefix}isimdeğiştir @${client.user.username}#${client.user.discriminator} <yeni isim>\``)
-if(isim.length > 32) return message.reply(`Lütfen \`32\` karakteri geçmeyecek şekilde bir isim giriniz!`)
+if(!isim) return message.reply(`${basarisiz} Lütfen bir kullanıcı adı giriniz! \nDoğru Kullanım; \`${prefix}isim @üye <yeni isim>\``)
+if(isim.length > 32) return message.reply(`${basari} Lütfen \`32\` karakteri geçmeyecek şekilde bir isim giriniz!`)
   
 message.guild.members.cache.get(kullanici.id).setNickname(`${tag} ${isim}`)
 message.channel.send(`${basari} Başarılı bir şekilde \`${kullanici.username}\` adlı kişinin kullanıcı adı \`${tag} ${isim}\` olarak değiştirildi.`)
@@ -25,7 +25,7 @@ message.channel.send(`${basari} Başarılı bir şekilde \`${kullanici.username}
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['isimdegistir','ideğiş','idegis','isim'],
+    aliases: ['isimdegistir','ideğiş','idegis','isim','i'],
     permLevel: 0
 }
 
