356095b3642762b33c994477e969f6ad346ce050
Glitch (planta-mod) <none>
2021-03-25 07:03:42 +0000

    📰🖼 Checkpoint ./komutlar/rolver.js:26490528/922

--- komutlar/rolver.js
+++ komutlar/rolver.js
@@ -16,15 +16,17 @@ exports.run = async (bot, message, args) => {
      let basarili = ayarlar.basariliemoji;
      let basarisiz = ayarlar.basarisizemoji;
      let yetkili = ayarlar.logger;
+     
 
   
- if (!message.member.roles.cache.get(yetkili) & !message.member.hasPermission("ADMINISTRATOR")) return message.react(basarisiz);
+ if (!message.member.roles.cache.get(yetkili) & !message.member.hasPermission("ADMINISTRATOR"));
 
   
-  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[0]);
+    let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[0]);
     if (!rMember) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} Rol vermem için bir kişiyi etiketlemelisin!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
-    let role = message.mentions.roles.first()
+    let role = message.mentions.roles.first() || message.guild.roles.cache.find(a => a.name == args.slice(1).join(" "));
 
+    if (message.member.roles.highest.comparePositionTo(role) < 1) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} Vericeğin rol senden daha üstün!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
     if (!role) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} Rol verebilmem için rolü belirtmelisiniz!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
     let aRole = message.mentions.roles.first()
     if (!aRole) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} Etiketlediğiniz rolü sunucuda bulamıyorum!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
@@ -43,7 +45,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: "",
+  name: "rolver",
   description: "Kişilere Rol Yetkisi Verir",
-  usage: " <mesaj>"
+  usage: "rolver <mesaj>"
 };
