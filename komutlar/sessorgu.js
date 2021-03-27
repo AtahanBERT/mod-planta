b1a06dbd4362a0d112043cbd5d611997d36758d3
Glitch (planta-mod) <none>
2021-03-27 10:07:36 +0000

    🙏🌦 Checkpoint ./komutlar/sessorgu.js:26490528/3782

--- komutlar/sessorgu.js
+++ komutlar/sessorgu.js
@@ -1,52 +1,47 @@
 const Discord = require("discord.js");
-const { MessageEmbed } = require("discord.js");
+const ayarlar = require("../ayarlar.json");
 const db = require("quick.db");
+let basari = ayarlar.basariliemoji;
+let basarisiz = ayarlar.basarisizemoji;
 
 exports.run = async (client, message, args) => {
   let member =
     message.mentions.members.first() || message.guild.members.cache.get(args[0]);
   if (!member)
     return message.channel.send(
-      new MessageEmbed()
+      new Discord.MessageEmbed()
         .setDescription("Bir Kullanıcı Etiketle")
         .setColor("DARK_PURPLE")
     );
   if (!member.voice.channel)
-    return message.reply("Bu Kullanıcı Ses Kanalında Değil");
+    return message.channel.send(new Discord.MessageEmbed(`${basarisiz} Bu Kullanıcı Ses Kanalında Değil`))
     let seskanali = member.voice.channel.name;
 
     if (message.member.voice.selfDeaf == true) {
-      let sorgu = new MessageEmbed()
+      let sorgu = new Discord.MessageEmbed()
         .setColor("GOLD")
         .setAuthor(message.author.tag, message.author.avatarURL())
   
         .setDescription(
-          `${member} İsimli Kişi **${seskanali}** Ses Kanalında Şuan Aktif Ve Kulaklığı Kapalı`
-        );
-  
-       if (message.member.voice.selfMute == true) {
-      let sorgu = new MessageEmbed()
-        .setColor("GOLD")
-        .setAuthor(message.author.tag, message.author.avatarURL())
-  
-        .setDescription(
-          `${member} İsimli Kişi **${seskanali}** Ses Kanalında Şuan Aktif Ve Kulaklığı Kapalı`
-        );
+          `${basari} ${member} İsimli Kişi **${seskanali}** Ses Kanalında Şuan Aktif Ve Kulaklığı Kapalı`
+        )
+      message.react(basari);
       
       message.channel.send(sorgu);
     } else {
-      let sorguu = new MessageEmbed()
+      let sorguu = new Discord.MessageEmbed()
         .setColor("GREY")
         .setAuthor(message.author.tag, message.author.avatarURL())
         .setDescription(
-          `${member} İsimli Kişi **${seskanali}** Ses Kanalında Şuan Aktif Ve Kulaklığı Açık`
-        );
+          `${basari} ${member} İsimli Kişi **${seskanali}** Ses Kanalında Şuan Aktif Ve Kulaklığı Açık`
+        )
+      message.react(basari);
   
       message.channel.send(sorguu);
     }
   
     if (!member.voice.channel)
-      return message.reply("Ses Kanalında Yok");
+      return message.channel.send(new Discord.MessageEmbed(`${basarisiz} Ses Kanalında Yok`))
   };
 
 exports.conf = {
