a8d7b890f26e1474234d94f2a546e2a96824fb3a
Glitch (planta-mod) <none>
2021-03-29 16:34:47 +0000

    🐿🍔 Checkpoint ./komutlar/tag-info.js:26490528/16

--- komutlar/tag-info.js
+++ komutlar/tag-info.js
@@ -7,7 +7,7 @@ let basari = ayarlar.basariliemoji;
 module.exports.run= async(client, message, args) => {
   
 const tag = args.slice(0).join(" ")
-if(!tag) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir Tag Belirt!`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp())
+if(!tag) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir Tag Belirt!`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x800d0d').setTimestamp())
 const sonuc = message.guild.members.cache.filter(mr => mr.user.username.includes(tag)).size
 
 message.channel.send(new MessageEmbed().setDescription(`${basari} ${message.author}, Belirtilen Taga Sahip Bu Sunucuda `+sonuc+` Kişi Var!`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp())
