750790a657b5d73744eee74ecb4d0fd7c8f812a0
Glitch (planta-mod) <none>
2021-03-29 16:35:16 +0000

    Rewound to commit cb0d2db0bb965a00cecc6fde65dc6382bed07e40 + 0 edits

--- komutlar/tag-info.js
+++ komutlar/tag-info.js
@@ -1,16 +1,10 @@
-const Discord = require("discord.js");
-const { MessageEmbed } = require("discord.js");
-const ayarlar = require("../ayarlar.json");
-let basarisiz = ayarlar.basarisizemoji;
-let basari = ayarlar.basariliemoji;
-
+const Discord = require("discord.js")
 module.exports.run= async(client, message, args) => {
-  
-const tag = args.slice(0).join(" ")
-if(!tag) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir Tag Belirt!`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x800d0d').setTimestamp())
-const sonuc = message.guild.members.cache.filter(mr => mr.user.username.includes(tag)).size
+const cst = args.slice(0).join(" ")
+if(!cst) return message.reply("Bir Tag Belirt!")
+const sonuc = message.guild.members.cache.filter(mr => mr.user.username.includes(cst)).size
 
-message.channel.send(new MessageEmbed().setDescription(`${basari} ${message.author}, Belirtilen Taga Sahip Bu Sunucuda `+sonuc+` Kişi Var!`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp())
+message.reply("Belirtilen Taga Sahip Bu Sunucuda `"+sonuc+"` Kişi Var!")
 }
 module.exports.conf = {
 aliases: []
@@ -18,4 +12,4 @@ aliases: []
 
 module.exports.help = {
 name: "tag-info"
-};
+}
