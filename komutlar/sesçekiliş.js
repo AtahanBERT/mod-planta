f99d28454acaafddc07ca0e297748a15edaf3f5c
Glitch (planta-mod) <none>
2021-04-02 19:37:18 +0000

    💕👾 Checkpoint ./komutlar/sesçekiliş.js:26490528/4

--- komutlar/sesçekiliş.js
+++ komutlar/sesçekiliş.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, message, args) => {
 if (!message.member.roles.cache.get(ayarlar.rewardsyetkili)) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
   if (!message.member.voice.channel)
     return message.channel.send(new Discord.MessageEmbed().setDescription(
-      `${basarisiz} **Bu Komutu Kullana Bilmek İçin Sesli Kanalda Olman Gerekli!**`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+      `${basarisiz} Bu Komutu Kullana Bilmek İçin Sesli Kanalda Olman Gerekli!`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
   let odul = args.slice(0).join(" ");
   if (!odul) return message.reply("**Bir Ödül Yazman Gerek!**");
 
