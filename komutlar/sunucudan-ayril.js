f4844d6de5f7e098f574b3f733c5b3f301c5584a
Glitch (planta-mod) <none>
2021-04-09 21:22:26 +0000

    🎳🚲 Checkpoint ./package.json:26490528/4305 ./komutlar/sunucudan-ayril.js:26490528/939

--- komutlar/sunucudan-ayril.js
+++ komutlar/sunucudan-ayril.js
@@ -7,23 +7,23 @@ let basari = ayarlar.basariliemoji;
 
 exports.run = async (bot, message, args, client) => {
    const id = require('quick.db')
-   if(message.author.id !== ayarlar.sahip) return message.channel.send(`${basarisiz} Hey Sen Benim Sahibim Değilsin!`);
+   if(message.author.id !== ayarlar.sahip) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
  
   let guildid = args[0]
-  if(!guildid) return message.channel.send(`${basarisiz} Ayrılınacak sunucunun id sini girmelisin!`)
+  if(!guildid) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author} Ayrılınacak Sunucunun İD Sini Girmelisin.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
 bot.guilds.cache.get(guildid).leave();
 const embed = new Discord.MessageEmbed()
  
-.addField(`${basari} İşlem Başarılı ','Başarıyla sunucudan Ayrıldım`)
+.addField(`İşlem Başarılı ','Başarıyla sunucudan Ayrıldım`)
 .addField(`Ayrıldığım Sunucunun İD si:`,`ID: ${guildid}`)
-.setFooter('Planta Team a Aittir.')
+.setFooter('Extacy Community e Aittir.')
 message.channel.send(embed)
  
 }
 exports.conf = {
   enabled:true,
   guildOnly:false,
-  aliases : ['sunucu-ayril'],
+  aliases : ['sunucu-ayril','çık','çik'],
   permLevel:0
 };
 exports.help = {
