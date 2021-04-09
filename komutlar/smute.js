8fb2555e462dfe290e0ce7b8102d060f1acb67b9
Glitch (planta-mod) <none>
2021-04-09 07:15:31 +0000

    👚🐛 Checkpoint ./komutlar/smute.js:26490528/50 ./komutlar/mute.js:26490528/35

--- komutlar/smute.js
+++ komutlar/smute.js
@@ -22,7 +22,7 @@ exports.run = async (client ,message, args) =>{
       let mutezaman = args[1];
       let sebep = args.slice(2,args.length).join(" ");
 
-    if (!message.member.hasPermission("ADMINISTRATOR") & !message.member.roles.cache.get(yetkili) ) return message.react(basarisiz).then(x => x.delete({timeout: 3000})); //acebots  
+    if (!message.member.hasPermission("ADMINISTRATOR") & !message.member.roles.cache.get(yetkili) ) return message.react(basarisiz); 
     if (!mutekisi) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:plantacarp:815252488168931368> Lütfen bir kullanıcı etiketleyiniz!`)).then(x => x.delete({timeout: 5000}));
     if (message.member.roles.highest.position <= mutekisi.roles.highest.position) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:plantacarp:815252488168931368> Belirttiğin kişi senden üstün veya onunla aynı yetkidesin!`)).then(x => x.delete({timeout: 5000}));
     if (!mutezaman) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:plantacarp:815252488168931368> Lütfen bir zaman giriniz! \n 1 Saniye = 1s \n 1 Dakika = 1m \n 1 Saat = 1h \n 1 Gün = 1d`));
