358243c952bcb408ebce1dbfe258c45d8df0f955
Glitch (planta-mod) <none>
2021-04-11 16:58:38 +0000

    👻🏃 Checkpoint ./events/message.js:26490528/1665

--- events/message.js
+++ events/message.js
@@ -51,50 +51,10 @@ if (!client.commands.has(command)) {
       cmd = client.commands.get(client.aliases.get(command));
     } else {
       if(command == '') return;
-const kraker = new Discord.MessageEmbed()
-.setDescription("Botta `" + command + '` Adında Bir Komut Bulunamadı.')
+message.channel.send(new Discord.MessageEmbed()
+.setDescription(`${basarisiz} Botta ` + command + ' Adında Bir Komut Bulunamadı.')
 .setColor('#2c2f33')
-.setTimestamp()
-    message.reply(kraker)
-  }
-  };
-  
-  if (client.commands.has(command)) {
-    cmd = client.commands.get(command);
-  } else if (client.aliases.has(command)) {
-    cmd = client.commands.get(client.aliases.get(command));
-  }
-  if (cmd) {
-    if (perms < cmd.conf.permLevel) return;
-    cmd.run(client, message, params, perms);
-  }
-};
-
-module.exports = message => {
-  if (talkedRecently.has(message.author.id)) {
-    return;
-  }
-  talkedRecently.add(message.author.id);
-	setTimeout(() => {
-    talkedRecently.delete(message.author.id);
-  }, 0);
-  let client = message.client;
-  if (message.author.bot) return;
-  if (!message.content.startsWith(ayarlar.prefix)) return;
-  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
-  let params = message.content.split(' ').slice(1);
-  let perms = client.elevation(message);
-  let cmd;
-  if (client.commands.has(command)) {
-    cmd = client.commands.get(command);
-  } else if (client.aliases.has(command)) {
-    cmd = client.commands.get(client.aliases.get(command));
-  }
-  if (cmd) {
-    if (perms < cmd.conf.permLevel) return;
-    cmd.run(client, message, params, perms);
-  }
+.setTimestamp())
+  }}
 
 };
-
-
