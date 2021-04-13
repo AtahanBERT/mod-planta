5f58c1045e55d10d608a97d66b92fbf1612ab6c5
Glitch (planta-mod) <none>
2021-04-13 21:49:59 +0000

    🐵🎓 Checkpoint ./komutlar/dm-at.js:26490528/163

--- komutlar/dm-at.js
+++ komutlar/dm-at.js
@@ -3,18 +3,19 @@ const { MessageEmbed } = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 let basarisiz = ayarlar.basarisizemoji;
 let basari = ayarlar.basariliemoji;
+let yetkili = ayarlar.logger;
 
 exports.run = async(client, message, args) => {
   
-if (!message.member.roles.cache.get(ayarlar.logger) & !message.member.hasPermission("ADMINISTRATOR"))
+if (!message.member.roles.cache.get(yetkili) & !message.member.hasPermission("ADMINISTRATOR"))
 return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
 
-let kullanici = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));  
-if (!wen) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir kullanıcı ID si gir.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
-let samita = args.slice(1).join(" ")
-if(!samita) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir mesaj belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
-wen.send(samita)
-message.react(basari);
+let kullanıcı = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));  
+if (!kullanıcı) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir kullanıcı Etiketle veya ID gir.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+let extacy = args.slice(1).join(" ")
+if(!extacy) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir mesaj belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+kullanıcı.send(extacy)
+message.react('✅');
 };
 
 exports.conf = {
