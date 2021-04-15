8e651e5c2732739b76e516bdbb2375a78423a44a
Glitch (planta-mod) <none>
2021-04-15 15:17:47 +0000

    🚎🌑 Checkpoint ./komutlar/isim.js:26490528/7

--- komutlar/isim.js
+++ komutlar/isim.js
@@ -24,7 +24,7 @@ if(!yaş) return message.channel.send(new Discord.MessageEmbed().setDescription(
 if(isim.length > 32) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} Lütfen \`32\` karakteri geçmeyecek şekilde bir isim giriniz!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
   
 message.guild.members.cache.get(kullanici.id).setNickname(`${tag} ${isim} ${yaş}`)
-message.channel.send(new Discord.MessageEmbed().setDescription(`${basari} Başarılı bir şekilde ${kullanici} adlı kişinin kullanıcı adı \`${tag} ${isim} ${yaş}\` olarak değiştirildi.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp()).then(x => x.delete({timeout: 5000}))
+message.channel.send(new Discord.MessageEmbed().setDescription(`${basari} Başarılı bir şekilde ${kullanici} adlı kişinin kullanıcı adı \`${isim} ${yaş}\` olarak değiştirildi.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp()).then(x => x.delete({timeout: 5000}))
 message.react('✅');
 }
 
