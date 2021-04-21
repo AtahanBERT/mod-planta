6e3e788ea037f79b058152054882ef01266d7bb5
Glitch (planta-mod) <none>
2021-04-21 20:57:54 +0000

    📯🚲 Checkpoint ./komutlar/dm-at.js:26490528/641

--- komutlar/dm-at.js
+++ komutlar/dm-at.js
@@ -10,7 +10,7 @@ exports.run = async(client, message, args) => {
 if (!message.member.roles.cache.get(yetkili) & !message.member.hasPermission("ADMINISTRATOR"))
 return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
 
-let kullanıcı = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.member.id)
+let kullanıcı = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]))
 if (!kullanıcı) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir kullanıcı Etiketle veya İD gir.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
 let extacy = args.slice(1).join(" ")
 if(!extacy) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir mesaj belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
