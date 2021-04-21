560dd8804b53ffe24efd33f72d829c518e203917
Glitch (planta-mod) <none>
2021-04-21 17:04:59 +0000

    🏢🧀 Checkpoint ./komutlar/gel.js:26490528/460

--- komutlar/gel.js
+++ komutlar/gel.js
@@ -8,9 +8,9 @@ exports.run = async (client, message, emoji, args) => {
   if(message.author.id !== ayarlar.sahip) {return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`${basarisiz} Şuanda bot kullanımı kapalıdır. Daha sonra tekrar deneyiniz.`))}
 }
   
-if (!message.member.voice.channel) {
-return message.reply("Ses kanalında olman lazım!");
-}
+if (!message.member.voice.channel) 
+return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Ses kanalında olman lazım!`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+
 const filter = (reaction, user) => {
 return ['✅', '❌'].includes(reaction.emoji.name) && user.id === kullanıcı.id;
 };
@@ -18,11 +18,11 @@ return ['✅', '❌'].includes(reaction.emoji.name) && user.id === kullanıcı.i
 let basarisiz = ayarlar.basarisizemoji
 let basari = ayarlar.basariliemoji
 let kullanıcı = message.mentions.members.first()
-if (!kullanıcı) return message.channel.send((`${basarisiz} Bir Kullanıcı Belirt.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+if (!kullanıcı) return message.channel.send((`${basarisiz} ${message.author}, Lütfen Bir Kullanıcı Belirt.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
 
 let member = message.guild.member(kullanıcı);
 
-if (!member.voice.channel) return message.channel.send((`${basarisiz} Etiketlenen Kullanıcı Ses Kanalında Değil.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+if (!member.voice.channel) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Etiketlenen Kullanıcı Ses Kanalında Değil.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
 
 const voiceChannel = message.member.voice.channel.id;
 if (!voiceChannel) return;
@@ -44,12 +44,12 @@ const reaction = collected.first();
 if (reaction.emoji.name === '✅') {
 let kabul = new Discord.MessageEmbed()
 .setColor("0x348f36")
-.setDescription(`${basari} ${kullanıcı} Odaya Çekilme Teklifini Onayladı`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+.setDescription((`${basari} ${message.author}, ${kullanıcı} Odaya Çekilme Teklifini Onayladı`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
 message.channel.send(kabul)
 kullanıcı.voice.setChannel(message.member.voice.channel.id)
 } else {
 let striga = new Discord.MessageEmbed()
-.setDescription((`${basarisiz} ${kullanıcı} Odaya Çekilme Teklifini Reddetti`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+.setDescription((`${basarisiz} ${message.author}, ${kullanıcı} Odaya Çekilme Teklifini Reddetti`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
 message.channel.send(striga)
 }
 })
