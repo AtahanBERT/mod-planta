4c9a44479a4aa8c600be8db3eb5b6d50440ad7db
Glitch (planta-mod) <none>
2021-04-22 18:10:55 +0000

    ⛴💐 Checkpoint ./komutlar/owo-claim.js:26490528/63 ./komutlar/uyar.js:26490528/620

--- komutlar/owo-claim.js
+++ komutlar/owo-claim.js
@@ -7,13 +7,11 @@ let basarisiz = ayarlar.basarisizemoji;
 exports.run = async(client, message) => {
 if (!message.member.roles.cache.get(yetki)) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
       let addrole = "823592578867920906";
-      let removerole = ""
       let usize = message.mentions.users.array().length;
       let u = message.mentions.users.array();
       for(let i=0; i < usize; i++){
         let gu = message.guild.members.cache.find(r => r.id == u[i].id)
         gu.roles.add(addrole);
-        gu.roles.remove(removerole);
         message.react('✅');
       }
       //console.log(usize + " USERS\n" + u)
