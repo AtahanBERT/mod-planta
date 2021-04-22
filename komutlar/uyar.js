9a9c728680fbd317695acbab9a34ece20326cd06
Glitch (planta-mod) <none>
2021-04-22 00:47:50 +0000

    ✌️🚈 Checkpoint ./komutlar/uyar.js:26490528/320

--- komutlar/uyar.js
+++ komutlar/uyar.js
@@ -1,5 +1,5 @@
 const { MessageEmbed } = require("discord.js");
-const db = require('quick.db');
+const db = require("quick.db");
 const ayarlar = require("../ayarlar.json");
 let basari = ayarlar.basariliemoji;
 let basarisiz = ayarlar.basarisizemoji;
@@ -10,12 +10,17 @@ if (!message.member.roles.cache.get(ayarlar.logger) & !message.member.hasPermiss
 return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
 
  let uyarı1 = ayarlar.uyarı1;
- let uyarisayisi = db.fetch(`uyarı_${uyarılcak.id}`);
+ let uyarı2 = ayarlar.uyarı2;
+ let uyarisayisi = await db.fetch(`uyari_${uyarilcak}`);
  let sunucu = message.guild;
- let uyarılcak = message.mentions.users.first() || message.guild.members.cache.get(args[0])
+ let uyarilcak = (message.mentions.users.first() || message.guild.members.cache.get(args[0]));
  let sebep = args.slice(1).join(" ");
+ 
+if (uyarisayisi === null) { uyarilcak.roles.add(uyarı1) }
+if (uyarisayisi === 1) { uyarilcak.roles.add(uyarı2) }
+ 
 
-if(!uyarılcak)
+if(!uyarilcak)
   return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir kullanıcı etiketlemelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}))
 
 if(!sebep)
@@ -23,13 +28,11 @@ if(!sebep)
 
   
  else
- message.channel.send(new MessageEmbed().setDescription(`${basari} ${message.author}, ${uyarılcak} Adlı kişiyi başarıyla uyardım, özel mesajlarında uyarısı gözükücektir.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp())
-  return uyarılcak.send(new MessageEmbed().setDescription(`${sunucu}, Sunucusunda \`${sebep}\` Sebebiyle Uyarıldın!`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('BLACK').setTimestamp())
-message.react('✅');
+ message.channel.send(new MessageEmbed().setDescription(`${basari} ${message.author}, ${uyarilcak} Adlı kişiyi başarıyla uyardım, özel mesajlarında uyarısı gözükücektir.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp());
+ db.add(`uyari_${uyarilcak}`, 1);
+ return uyarilcak.send(new MessageEmbed().setDescription(`${sunucu}, Sunucusunda \`${sebep}\` Sebebiyle Uyarıldın!`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('BLACK').setTimestamp())
+ message.react('✅');
   
-db.add(`uyarı_${uyarılcak.id}`, 1); 
-if (uyarisayisi === null) { uyarılcak.roles.add(uyarı1) }
-if (uyarisayisi === 1) { uyarılcak.roles.add(uyarı2) }
 
 };
 
