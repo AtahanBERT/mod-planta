04aec6c3e6da27a2189e11cd3ffa0d82c575f1d6
Glitch (planta-mod) <none>
2021-04-22 17:24:44 +0000

    👬🕴 Checkpoint ./komutlar/uyar.js:26490528/512

--- komutlar/uyar.js
+++ komutlar/uyar.js
@@ -12,37 +12,19 @@ return message.channel.send(new Discord.MessageEmbed().setDescription(`${basaris
  let uyarı1 = ayarlar.uyarı1;
  let uyarı2 = ayarlar.uyarı2;
  let uyarı3 = ayarlar.uyarı3;
- let uyarisayisi = await db.fetch(`uyari.sayi_${uyarilcak.id}`);
  let sunucu = message.guild;
  let uyarilcak = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
  let sebep = args.slice(1).join(" ");
 
-if(!uyarilcak)
-  return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir kullanıcı etiketlemelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}))
+if(!uyarilcak) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir kullanıcı etiketlemelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}))
 
-if(!sebep)
-  return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir sebep belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+if(!sebep) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir sebep belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
 
   
- else
+
  message.channel.send(new Discord.MessageEmbed().setDescription(`${basari} ${message.author}, ${uyarilcak} Adlı kişiyi başarıyla uyardım, özel mesajlarında uyarısı gözükücektir.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp());
  return uyarilcak.send(new Discord.MessageEmbed().setDescription(`${sunucu}, Sunucusunda \`${sebep}\` Sebebiyle Uyarıldın!`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('BLACK').setTimestamp())
  message.react('✅');
-
-db.add(`uyari.sayi_${uyarilcak.id}`, 1);
-if (uyarisayisi === null) return uyarilcak.roles.add(uyarı1)
-  
-if (uyarisayisi === 1) return uyarilcak.roles.add(uyarı2)
-  
-if (uyarisayisi === 2) return uyarilcak.roles.add(uyarı3)
-  
-if (uyarisayisi === 3) return
-uyarilcak.roles.cache.forEach(r => {
-uyarilcak.roles.remove(r.id);
-uyarilcak.roles.add(ayarlar.cezalı);
-
-});
-
 };
 
 exports.conf = {
