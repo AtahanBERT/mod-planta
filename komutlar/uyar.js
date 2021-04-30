2652fba04c30531e8b6a3f47bfbeca7db9553068
Glitch (planta-mod) <none>
2021-04-30 16:34:24 +0000

    🌃🍼 Checkpoint ./komutlar/uyar.js:26490528/7291

--- komutlar/uyar.js
+++ komutlar/uyar.js
@@ -5,57 +5,137 @@ let basari = ayarlar.basariliemoji;
 let basarisiz = ayarlar.basarisizemoji;
 
 exports.run = async (client, message, args) => {
- 
-if (!message.member.roles.cache.get(ayarlar.logger) & !message.member.hasPermission("ADMINISTRATOR"))
-return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+  if (
+    !message.member.roles.cache.get(ayarlar.logger) &
+    !message.member.hasPermission("ADMINISTRATOR")
+  )
+    return message.channel
+      .send(
+        new MessageEmbed()
+          .setDescription(
+            `${basarisiz} ${message.author} Komutu kullanmak için yetkin bulunmamakta.`
+          )
+          .setColor("0x800d0d")
+          .setAuthor(
+            message.member.displayName,
+            message.author.avatarURL({ dynamic: true })
+          )
+          .setTimestamp()
+      )
+      .then(x => x.delete({ timeout: 5000 }));
 
- let uyarı1 = ayarlar.uyarı1;
- let uyarı2 = ayarlar.uyarı2;
- let uyarı3 = ayarlar.uyarı3;
- let kullanıcı = message.mentions.users.first()
- let uyarisayisi = await db.fetch(`uyari.${message.guild.id}.${kullanıcı.id}`);
- let sunucu = message.guild;
- let uyarilcak = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
- let sebep = args.slice(1).join(" ");
+  let uyarı1 = ayarlar.uyarı1;
+  let uyarı2 = ayarlar.uyarı2;
+  let uyarı3 = ayarlar.uyarı3;
 
-if(!uyarilcak)
-  return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir kullanıcı etiketlemelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}))
+  let uyarisayisi = await db.fetch(`uyari.${message.guild.id}.${uyarilcak.id}`);
+  let sunucu = message.guild;
+  let uyarilcak =
+    message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+  let sebep = args.slice(1).join(" ");
 
-if(!sebep)
-  return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir sebep belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+  if (!uyarilcak)
+    return message.channel
+      .send(
+        new MessageEmbed()
+          .setDescription(
+            `${basarisiz} ${message.author}, Bir kullanıcı etiketlemelisin.`
+          )
+          .setAuthor(
+            message.member.displayName,
+            message.author.avatarURL({ dynamic: true })
+          )
+          .setColor("0x800d0d")
+          .setTimestamp()
+      )
+      .then(x => x.delete({ timeout: 5000 }));
 
-  
- else
- message.channel.send(new MessageEmbed().setDescription(`${basari} ${message.author}, ${uyarilcak} Adlı kişiyi başarıyla uyardım, özel mesajlarında uyarısı gözükücektir.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp());
- return uyarilcak.send(new MessageEmbed().setDescription(`${sunucu}, Sunucusunda \`${sebep}\` Sebebiyle Uyarıldın!`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('BLACK').setTimestamp())
- message.react('✅');
+  if (!sebep)
+    return message.channel
+      .send(
+        new MessageEmbed()
+          .setDescription(
+            `${basarisiz} ${message.author}, Bir sebep belirtmelisin.`
+          )
+          .setAuthor(
+            message.member.displayName,
+            message.author.avatarURL({ dynamic: true })
+          )
+          .setColor("0x800d0d")
+          .setTimestamp()
+      )
+      .then(x => x.delete({ timeout: 5000 }));
+  else
+    message.channel.send(
+      new MessageEmbed()
+        .setDescription(
+          `${basari} ${message.author}, ${uyarilcak} Adlı kişiyi başarıyla uyardım, özel mesajlarında uyarısı gözükücektir.`
+        )
+        .setAuthor(
+          message.member.displayName,
+          message.author.avatarURL({ dynamic: true })
+        )
+        .setColor("0x348f36")
+        .setTimestamp()
+    );
+  return uyarilcak.send(
+    new MessageEmbed()
+      .setDescription(
+        `${sunucu}, Sunucusunda \`${sebep}\` Sebebiyle Uyarıldın!`
+      )
+      .setAuthor(
+        message.member.displayName,
+        message.author.avatarURL({ dynamic: true })
+      )
+      .setColor("BLACK")
+      .setTimestamp()
+  );
+  message.react("✅");
 
-db.add(`uyari.${message.guild.id}.${kullanıcı.id}`, 1)
-if (uyarisayisi === null) {return uyarilcak.roles.add(uyarı1)}
-  
-if (uyarisayisi === 1) {return uyarilcak.roles.add(uyarı2)}
-  
-if (uyarisayisi === 2) {return uyarilcak.roles.add(uyarı3)}
-  
-if (uyarisayisi === 3) {return
-uyarilcak.roles.cache.forEach(r => {
-uyarilcak.roles.remove(r.id);
-uyarilcak.roles.add(ayarlar.cezalı)
-db.delete(`uyari.${message.guild.id}.${kullanıcı.id}`)
-message.channel.send(new MessageEmbed().setDescription(``).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp());
-})}
+  db.add(`uyari.${message.guild.id}.${uyarilcak.id}`, +1);
+  if (uyarisayisi === null) {
+    return uyarilcak.roles.add(uyarı1);
+  }
 
+  if (uyarisayisi === 1) {
+    return uyarilcak.roles.add(uyarı2);
+  }
+
+  if (uyarisayisi === 2) {
+    return uyarilcak.roles.add(uyarı3);
+  }
+
+  if (uyarisayisi === 3) {
+    uyarilcak.roles.cache.forEach(r => {
+      uyarilcak.roles.remove(r.id);
+      uyarilcak.roles.add(ayarlar.cezalı);
+      db.delete(`uyari.${message.guild.id}.${uyarilcak.id}`);
+      return message.channel.send(
+        new MessageEmbed()
+          .setDescription(
+            `${basari} ${message.author}, ${uyarilcak} Adlı kişi \`3\` kez uyarıldığı için başarıyla jaile attım.`
+          )
+          .setAuthor(
+            message.member.displayName,
+            message.author.avatarURL({ dynamic: true })
+          )
+          .setColor("0x348f36")
+          .setTimestamp()
+      );
+    });
+  }
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ["uyarı", "uyarıver","uyari"],
+  aliases: ["uyarı", "uyarıver", "uyari"],
   permlevel: 0
 };
 
 exports.help = {
   name: "uyar",
-  description: "Belirtilen kullanıcıyı özel mesajlarında gözükecek şekilde uyarır.",
+  description:
+    "Belirtilen kullanıcıyı özel mesajlarında gözükecek şekilde uyarır.",
   usage: "uyarı"
 };
