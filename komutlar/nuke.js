b0ff542eba733100f9a189ea76cda9cc4f4f57c0
Glitch (planta-mod) <none>
2021-05-06 16:00:22 +0000

    🗺💗 Checkpoint ./komutlar/nuke.js:26490528/792

--- komutlar/nuke.js
+++ komutlar/nuke.js
@@ -2,21 +2,23 @@ const Discord = require('discord.js');
 const database = require('quick.db');
 const { Client, MessageEmbed } = require('discord.js');
 
-const embed = new MessageEmbed()
-.setTitle('BAŞARILI!')
-.setColor(0xff0000)
-.setDescription('KANAL BAŞARI İLE NUKELENDİ! & L4v1n14#0707');
 
-exports.run = async (client, message, args) => {// can#0002
+exports.off = async (client, message, args) => {
   
+let basari = "";
+let basarisiz = "";
 let channel = message.mentions.channels.first() || message.channel;
- await channel.clone({ reason: "Kanal Koruma Sistemi" }).then(async kanal => {
+let position = channel.position;
+channel.delete();
+await channel.clone({ reason: "nuke" }).then(async kanal => {
     if (channel.parentID != null) await kanal.setParent(channel.parentID);
     await kanal.setPosition(channel.position);
     if (channel.type == "category") await channel.guild.channels.cache.filter(k => k.parentID == channel.id).forEach(x => x.setParent(kanal.id));
-  });
-
-}; 
+  }).then(s => {
+s.setPosition(position);
+s.send(new MessageEmbed().setDescription(`${basari} ${message.author}, `).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp());
+});
+};
 exports.conf = {
   enabled: true,
   guildOnly: false,
