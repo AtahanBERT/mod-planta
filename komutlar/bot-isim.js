75a2d6c7b19cf8d19c41ccb8e330c4fbff66b2e2
Glitch (planta-mod) <none>
2021-05-08 01:49:00 +0000

    🏪📰 Checkpoint ./komutlar/bot-isim.js:26490528/50

--- komutlar/bot-isim.js
+++ komutlar/bot-isim.js
@@ -10,12 +10,12 @@ if (message.author.id != ayarlar.sahip)
 return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
 {
 
-let isim = args.slice(0).join(" ")
-if(!isim) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, İsmimi belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
-if(isim.length > 32) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Lütfen \`32\` karakteri geçmeyecek şekilde bir isim giriniz!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+let Atahan = args.slice(0).join(" ")
+if(!Atahan) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, İsmimi belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+if(Atahan.length > 32) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Lütfen \`32\` karakteri geçmeyecek şekilde bir isim giriniz!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
 
-client.user.setUsername(isim) 
-return message.channel.send(new Discord.MessageEmbed().setDescription(`${basari} ${message.author}, Başarılı bir şekilde ismim \`${isim}\` olarak değiştirildi.`).setFooter(`NOT: 3 Kezden Fazla Kullanınca Değişmeyebilir Biraz Sonra Tekrar Deneyin Olucaktır.`, message.guild.iconURL({dynamic: true})).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp())
+client.user.setUsername(Atahan) 
+return message.channel.send(new Discord.MessageEmbed().setDescription(`${basari} ${message.author}, Başarılı bir şekilde ismim \`${Atahan}\` olarak değiştirildi.`).setFooter(`NOT: 3 Kezden Fazla Kullanınca Değişmeyebilir Biraz Sonra Tekrar Deneyin Olucaktır.`, message.guild.iconURL({dynamic: true})).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp())
 .catch(error => {
 message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, İsmini çok hızlı değiştiriyosun!`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}))
 })}};
