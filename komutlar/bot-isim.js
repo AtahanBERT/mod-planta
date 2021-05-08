fe920079726f94f15f99179ea3aba75297a315a3
Glitch (planta-mod) <none>
2021-05-08 00:04:03 +0000

    🖼🏕 Checkpoint ./komutlar/banbilgi.js:26490528/93 ./komutlar/bot-isim.js:26490528/43

--- komutlar/bot-isim.js
+++ komutlar/bot-isim.js
@@ -14,9 +14,10 @@ let isim = args.slice(0).join(" ")
 if(!isim) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, İsmimi belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
 if(isim.length > 32) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Lütfen \`32\` karakteri geçmeyecek şekilde bir isim giriniz!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
 
-client.user.setUsername(isim)
-return message.channel.send(new Discord.MessageEmbed().setDescription(`${basari} ${message.author}, Başarılı bir şekilde ismim \`${isim}\` olarak değiştirildi.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp())
-return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, İsmimi çok hızlı değişiyorsun.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+try {client.user.setUsername(isim)
+return message.channel.send(new Discord.MessageEmbed().setDescription(`${basari} ${message.author}, Başarılı bir şekilde ismim \`${isim}\` olarak değiştirildi.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp());
+}
+catch(err) { message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, İsmimi çok hızlı değişiyorsun.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));}
 };
 }
 exports.conf = {
