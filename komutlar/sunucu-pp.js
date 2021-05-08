75ac609a0e64c10abda2ed96b4fdf2148dd77e1f
Glitch (planta-mod) <none>
2021-05-08 02:01:02 +0000

    🚲🏘 Checkpoint ./komutlar/banbilgi.js:26490528/2 ./komutlar/sunucu-pp.js:26490528/168 ./ayarlar.json:26490528/33

--- komutlar/sunucu-pp.js
+++ komutlar/sunucu-pp.js
@@ -5,7 +5,7 @@ let basarisiz = ayarlar.basarisizemoji;
 
 exports.run = async (client, message, args) => {
 
-if (ayarlar.whitelist.some(sahipler => message.author.id !== sahipler))
+if (message.author.id !== ayarlar.sahip)
 return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
 {
 
@@ -20,7 +20,7 @@ exports.conf = {
     enabled: true,
     guildOnly: false,
     aliases: ['sunucu-pp','server-av','sunucu-av','server-pp','server-resim'],
-    permLevel: 0
+    permLevel: 0,
 }
 
 exports.help = {
