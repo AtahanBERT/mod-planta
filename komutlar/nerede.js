ec40a65057f04c82def46e03115ef0f7fb7894f9
Glitch (planta-mod) <none>
2021-05-10 18:39:36 +0000

    🎨☎️ Checkpoint ./komutlar/nerede.js:26490528/4286

--- komutlar/nerede.js
+++ komutlar/nerede.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const db = require('quick.db');
 const ayarlar = require("../ayarlar.json");
 
-exports.run = async (client, message, args ) => {
+exports.run = async (client, message, args) => {
   
    if(db.fetch(`bakim`)) {
   if(message.author.id !== ayarlar.sahip) {return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`${basarisiz} Şuanda bot kullanımı kapalıdır. Daha sonra tekrar deneyiniz.`))}
@@ -10,26 +10,21 @@ exports.run = async (client, message, args ) => {
   
        let basarisiz = ayarlar.basarisizemoji;
        let basari = ayarlar.basariliemoji;
-       let kanal = ayarlar.botkomut;
        let yetkili = ayarlar.logger;
-
-  if(message.channel.id !== kanal) return message.react(basarisiz);
 
   
      let kullanıcı = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
   
-  if (!message.member.roles.cache.get(yetkili) & !message.member.hasPermission("ADMINISTRATOR")) return message.react(basarisiz);
-if (!kullanıcı) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`${basarisiz} Bir Kullanıcı Belirtmelisin!`));
+  if (!message.member.roles.cache.get(yetkili) & !message.member.hasPermission("ADMINISTRATOR"))
+  return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+  if(!kullanıcı) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir kullanıcı etiketlemelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
   let kullanıcıkanal = kullanıcı.voice.channel;
-
   
-
-  
-if (!ayarlar.sahip) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`${basarisiz} Sahibimin üzerinde komut kullanamazsın!`));
-if(!kullanıcıkanal) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`${basarisiz}  Etiketlediğin Kullanıcı Sesli Kanalda Değil!`));
+if (!ayarlar.sahip) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Sahibimin üzerinde komut kullanamazsın!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+if(!kullanıcıkanal) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Etiketlediğin Kullanıcı Sesli Kanalda Değil!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
 
 if(kullanıcıkanal) {
-message.channel.send(new Discord.MessageEmbed().setColor('GREEN').setDescription(`${basari} ${kullanıcı} adlı kullanıcı **${kullanıcıkanal}** kanalında bulunmakta!`))
+message.channel.send(new Discord.MessageEmbed().setDescription(`${basari} ${message.author}, ${kullanıcı} adlı kullanıcı **${kullanıcıkanal}** kanalında bulunmakta.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x348f36').setTimestamp())
 };
  };
 
