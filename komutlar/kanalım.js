fe94c27b9510c9a5dd8b22b2b10a928ccc5fa97e
Glitch (planta-mod) <none>
2021-05-16 19:45:56 +0000

    🎤🙉 Checkpoint ./komutlar/kanalım.js:26490528/169 ./komutlar/nerede.js:26490528/58

--- komutlar/kanalım.js
+++ komutlar/kanalım.js
@@ -11,13 +11,12 @@ exports.run = async (client, message, args) => {
        let basarisiz = ayarlar.basarisizemoji;
        let basari = ayarlar.basariliemoji;
   
-    let kullanıcıkanal = message.author.voice.channel;
+let kullanıcıkanal = message.author.voice.channel;
   
-if (!ayarlar.sahip) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Sahibimin üzerinde komut kullanamazsın!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
-if(!kullanıcıkanal) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Etiketlediğin Kullanıcı Sesli Kanalda Değil!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+if(!kullanıcıkanal) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir ses kanalında değilsin!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
 
 if(kullanıcıkanal) {
-message.channel.send(new Discord.MessageEmbed().setDescription(`${basari} ${message.author}, **${kullanıcıkanal}** `).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x348f36').setTimestamp())
+message.channel.send(new Discord.MessageEmbed().setDescription(`${basari} ${message.author}, Kanalıma katılmak istermisin işte sağdaki yere tıkla **${kullanıcıkanal}** ve gel!`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x348f36').setTimestamp())
 message.react('✅')
 };
  };
