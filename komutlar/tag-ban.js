789c30945ff14cbeebd2c4bc1c422b35e87acc77
Glitch (planta-mod) <none>
2021-05-25 16:49:08 +0000

    🍬🔖 Checkpoint ./komutlar/tag-ban.js:26490528/477

--- komutlar/tag-ban.js
+++ komutlar/tag-ban.js
@@ -11,6 +11,9 @@ const tag = args.slice(0).join(" ")
 if(!tag) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir Tag Belirt!`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x800d0d').setTimestamp())
 const sonuc = message.guild.members.cache.filter(mr => mr.user.username.includes(tag)).size
 
+
+const ban = message.guild.members.cache.forEach.includes(tag)
+ban.ban
 message.channel.send(new MessageEmbed().setDescription(`${basari} ${message.author}, Belirtilen Taga Sahip `+sonuc+` Kişi Banlandı!`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp())
 message.react('✅')
 }
