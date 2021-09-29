29e5fffb18f5eb3af9ea8778224c7809f35453aa
Glitch (planta-mod) <none>
2021-09-29 22:19:41 +0000

    🎥💖 Checkpoint

--- komutlar/dm-at.js
+++ komutlar/dm-at.js
@@ -21,7 +21,6 @@ let extacy = args.slice(1).join(" ")
 if(!extacy) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir mesaj belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
 message.delete();
 kullanıcı.send(new MessageEmbed().setDescription(extacy).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('#3498db').setFooter(`Baska Sorunuz Varsa Buraya Yazın.`, message.guild.iconURL({ dynamic: true, format: 'png', size: 1024 })).setTimestamp())
-message.react('✅');
 };
 
 exports.conf = {
