c67d70868848b4c89e8a42f1b01afc65830e1e96
Glitch (planta-mod) <none>
2021-10-19 09:03:15 +0000

    💶🔨 Checkpoint ./komutlar/bakım.js:26490528/315 ./komutlar/bakım-modu.js:26490528/14

--- komutlar/bakım.js
+++ komutlar/bakım.js
@@ -7,17 +7,17 @@ exports.run = (client, message, args) => {
   
   if(message.author.id !== ayarlar.sahip) return
   
-  if(!args[0]) return message.channel.send('Bakım modunu açmak için m!bakım aç')
+  if(!args[0]) return message.channel.send('Bakım modunu açmak/kapatmak için m!bakım aç/kapat')
   
   let basari = ayarlar.basariliemoji
   let basarisiz = ayarlar.basarisizemoji
+  
   if(args[0] === 'aç') {
-    if(db.fetch(`bakim`, 'acik')) return message.channel.send(`${basarisiz} Bakım modu zaten açık`)
     message.channel.send(`${basari} Bakım modu açıldı.`)
     db.set(`bakim`)
   }
+  
   if(args[0] === 'kapat'){
-    if(!db.fetch(`bakim`)) return message.channel.send(`${basarisiz} Bakım modu zaten kapalı.`)
     message.channel.send(`${basari} Bakım modu kapatıldı.`)
     db.delete(`bakim`)
   }
