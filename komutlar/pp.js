320f2b6d96285a550dfd393923f7d289b3b8cc05
Glitch (planta-mod) <none>
2022-02-16 01:16:43 +0000

    🐞🙇 Checkpoint ./komutlar/pp.js:26490528/198

--- komutlar/pp.js
+++ komutlar/pp.js
@@ -25,21 +25,21 @@ let embed = new Discord.MessageEmbed()
 .setAuthor(user.tag + '', user.displayAvatarURL())
 .setDescription(`**[[PNG]](${user.displayAvatarURL({ format: 'png', size: 1024 })})** | **[[JPEG]](${user.displayAvatarURL({ format: 'jpeg', size: 1024 })})** | **[[GIF]](${user.displayAvatarURL({ format: 'gif', size: 1024 })})** | **[[WEBP]](${user.displayAvatarURL({ format: 'webp', size: 1024 })})**`)
 .setImage(user.displayAvatarURL({dynamic: true, size: 1024}))
-.setColor("RANDOM")
+.setColor("0x348f36")
 message.channel.send(embed)
-
+message.react('✅')
 } else {
 
   let embed = new Discord.MessageEmbed()
 .setAuthor(user.tag + '', user.displayAvatarURL())
 .setDescription(`**[[PNG]](${user.displayAvatarURL({ format: 'png',  size: 1024 })})** | **[[JPEG]](${user.displayAvatarURL({ format: 'jpeg',  size: 1024 })})** | **~~[GIF]~~** | **[[WEBP]](${user.displayAvatarURL({ format: 'webp',  size: 1024 })})**`)
 .setImage(user.displayAvatarURL({dynamic: true, size: 1024}))
-.setColor("RANDOM")
+.setColor("0x348f36")
 message.channel.send(embed)
-
+message.react('✅')
 }
 }catch{
-  message.channel.send(new Discord.MessageEmbed().setColor("0x800d0d").setDescription(`${basarisiz} ${message.author}, Kullanıcıyı bulamadım!`));
+  message.channel.send(new Discord.MessageEmbed().setColor("0x800d0d").setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setTimestamp().setDescription(`${basarisiz} ${message.author}, Kullanıcıyı bulamadım!`));
   return;
 }
 
