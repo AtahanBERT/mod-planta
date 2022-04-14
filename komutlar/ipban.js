aa542ed06718130f249221122b769450fedb6c25
Glitch (planta-mod) <none>
2022-04-14 14:02:46 +0000

    🍔🐀 Checkpoint ./komutlar/ipban.js:26490528/49

--- komutlar/ipban.js
+++ komutlar/ipban.js
@@ -5,7 +5,7 @@ let basarisiz = ayarlar.basarisizemoji;
 let basari = ayarlar.basariliemoji;
  
 module.exports.run = async (client, message, args) => {
-    if (!message.member.roles.cache.get(ayarlar.banyetkili) & !message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+    if (!message.member.roles.cache.get(ayarlar.banyetkili)) return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
    
   if (!args[0]) 
         return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir kullanıcı İD si gir.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
