527d3bea022c657359be15611163390942f9ed68
Glitch (planta-mod) <none>
2022-04-14 15:46:11 +0000

    ☕️🐬 Checkpoint ./komutlar/ipban.js:26490528/1804

--- komutlar/ipban.js
+++ komutlar/ipban.js
@@ -11,6 +11,7 @@ module.exports.run = async (client, message, args) => {
         return message.channel.send(new MessageEmbed().setDescription(`${basarisiz} ${message.author}, Bir kullanıcı İD si gir.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
    
    const banlog = message.guild.channels.cache.find(c => c.id === ayarlar.banlog)//Ban log kanalı  
+   let user;
    let sebeb = args.slice(1).join(" ");
    let kullanici = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]))
    var seyfooo = args[0]
@@ -24,7 +25,7 @@ module.exports.run = async (client, message, args) => {
                }
                message.guild.ban(seyfooo, sebeb)
                    .then(async (member) => {
-                       let user;
+                       
                        if (member instanceof Discord.GuildMember) {
                            user = member.user;
                        }
@@ -34,11 +35,11 @@ module.exports.run = async (client, message, args) => {
                        else {
                            user = await client.fetchUser(member);
                        }
+                     })
                        moment.locale("tr")
                        banlog.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('RANDOM').setTimestamp().setDescription(`**Sunucudan Yasaklandı !**\n**Banlayan Yetkili:** ${message.author} (\`${message.author.id}\`)\n**Banlanan Üye:** ${kullanici.user.tag} (\`${kullanici.user.id}\`)\n**Sebep:** \`${sebeb}\`\n**Tarih:** \`${moment(Date.now()).add(3,"hours").format("HH:mm:ss DD MMMM YYYY")}\` `))
                        message.channel.send(new MessageEmbed().setDescription(`${message.author}, <@!${user.id}> sunucudan yasaklandı!`).setColor('0x348f36').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
                        message.react('✅');
-                     })
                    });
  
 }
